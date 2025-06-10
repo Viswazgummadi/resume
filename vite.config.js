f323d78d122622c5afe64e68a616477bb7839c34
Viswazgummadi <viswazgummadi@gmail.com>
2025-06-10 20:28:59 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/resume/", // <-- IMPORTANT: Add this line (e.g., "/my-resume-site/")
+  base: "/resume/", 
 });
